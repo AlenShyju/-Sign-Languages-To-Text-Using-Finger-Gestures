3e3040aeab8fbf9c7c433536aafae8228430c77b
AlenShyju <159281217+AlenShyju@users.noreply.github.com>
2024-04-24 11:35:02 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+# Importing Libraries
+
+import numpy as np
+
+import cv2
+import os, sys
+import time
+import operator
+
+from string import ascii_uppercase
+
+import tkinter as tk
+from PIL import Image, ImageTk
+
+from hunspell import Hunspell
+import enchant
+
+from keras.models import model_from_json
